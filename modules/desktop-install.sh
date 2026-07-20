f883e97f8bfdcac057f14d32ef8d31c9490d492b
Jeremy Passarelli <recordguy96@aol.com>
2026-07-20 19:38:25 -0400

    Removed silver-sddm package.

--- modules/desktop-install.sh
+++ modules/desktop-install.sh
@@ -43,7 +43,7 @@ EOF
 kde-plasma/*
 kde-frameworks/*
 EOF
-	emerge -qv sonicde-base/sonic-meta x11-misc/silver-sddm kde-apps/kde-apps-meta kde-apps/kdecore-meta kde-plasma/kwallet-pam kde-apps/kcalc kde-apps/kcharselect kde-apps/sweeper kde-misc/kweather sys-block/partitionmanager app-cdr/dolphin-plugins-mountiso kde-misc/kclock kde-misc/kdeconnect kde-apps/okular kde-apps/gwenview kde-apps/filelight kde-apps/ark kde-apps/ffmpegthumbs kde-apps/audiocd-kio kde-apps/kwalletmanager
+	emerge -qv sonicde-base/sonic-meta kde-apps/kde-apps-meta kde-apps/kdecore-meta kde-plasma/kwallet-pam kde-apps/kcalc kde-apps/kcharselect kde-apps/sweeper kde-misc/kweather sys-block/partitionmanager app-cdr/dolphin-plugins-mountiso kde-misc/kclock kde-misc/kdeconnect kde-apps/okular kde-apps/gwenview kde-apps/filelight kde-apps/ark kde-apps/ffmpegthumbs kde-apps/audiocd-kio kde-apps/kwalletmanager
 	if ask_yes_no "Do you want Dolphin to integrate with Git repositories?"; then
 		emerge -qv kde-apps/dolphin-plugins-git
 	fi
